8c44e9aa708f03ee9190af80752803ded5e66cc0
Maniccia <mariamaniccia@gmail.com>
2019-01-31 18:21:42 -0500

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,10 @@
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const password = "Billyjoel1";
+const keys = ("./keys.js");
+const passWord = keys.password;
+
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -12,10 +16,48 @@ const connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: password,
+  password: passWord,
   database: "bamazon_db"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
 });
+
+function fullDisplay() {
+    query = 'SELECT * FROM products';
+
+    // Make the db query
+    connection.query(query, function (err, data) {
+        if (err) throw err;
+
+        console.log('Existing Inventory: ');
+        console.log('...................\n');
+
+        let string = '';
+
+        for (var i = 0; i < data.length; i++) {
+            string = '';
+            string += 'Item: ' + data[i].ID + ', ';
+            string += 'Product Name: ' + data[i].product_name + ', ';
+            string += 'Department: ' + data[i].department_name + ', ';
+            string += 'Price: $' + data[i].price + '\n';
+
+            console.log(string);
+        }
+        console.log("-------------------------------------------------\n");
+})
+};
+
+
+function productNameSearch() {
+  var query = "SELECT product_name FROM products";
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].product_name);
+    }
+    
+  });
+}
+
+
