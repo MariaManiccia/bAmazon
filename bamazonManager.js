ef3e11091d3634f3adb354afe4efc69f195e114c
Maniccia <mariamaniccia@gmail.com>
2019-02-04 21:05:24 -0500

    low inventory add

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,38 +20,40 @@ connection.connect(function (err) {
 });
 
 // ask the user what they would like to do
-inquirer
-    .prompt([{
-            type: 'list',
-            name: 'options',
-            message: 'Hello! What would you like to do?',
-            choices: [
-                "View Products for Sale",
-                "View Low Inventory",
-                "Add to Inventory",
-                "Add New Product"
-            ]
-        }
+function startPage() {
+    inquirer
+        .prompt([{
+                type: 'list',
+                name: 'options',
+                message: 'Hello! What would you like to do?',
+                choices: [
+                    "View Products for Sale",
+                    "View Low Inventory",
+                    "Add to Inventory",
+                    "Add New Product"
+                ]
+            }
 
-    ])
-    .then(answers => {
+        ])
+        .then(answers => {
 
-        switch (answers.options) {
+            switch (answers.options) {
 
-            case "View Products for Sale":
-                productName();
-                break;
-            case "View Low Inventory":
-                // code block
-                break;
-            case "Add to Inventory":
-                // code block
-                break;
-            case "Add New Product":
-                // code block
-                break;
-        }
-    });
+                case "View Products for Sale":
+                    productName();
+                    break;
+                case "View Low Inventory":
+                    lowInventory();
+                    break;
+                case "Add to Inventory":
+                    // code block
+                    break;
+                case "Add New Product":
+                    // code block
+                    break;
+            }
+        });
+};
 
 // function to grab the products
 function productName() {
@@ -59,13 +61,37 @@ function productName() {
     var query = "SELECT * FROM products";
     connection.query(query, function (err, res) {
         for (var i = 0; i < res.length; i++) {
-             let info = "";
+            // grab and display the information
+            let info = "";
             info += "Item: " + res[i].ID + ", ";
             info += "Product Name: " + res[i].product_name + ", ";
             info += "Department: " + res[i].department_name + ", ";
-            info += "Price: $" + res[i].price+ ", ";
-            info += "Quantity:"+ res[i].stock_quantity+ "\n";
+            info += "Price: $" + res[i].price + ", ";
+            info += "Quantity:" + res[i].stock_quantity + "\n";
             console.log(info);
-          }
+        }
     })
 };
+
+function lowInventory() {
+
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+
+        for (var i = 0; i < res.length; i++) {
+
+            if (res[i].stock_quantity < 5) {
+                let lowQuant = "";
+                lowQuant += "Item: " + res[i].ID + ", ";
+                lowQuant += "Product Name: " + res[i].product_name + ", ";
+                lowQuant += "Quantity:" + res[i].stock_quantity + "\n";
+                console.log(lowQuant);
+            } else {
+                console.log("-");
+            }
+        }
+        startPage();
+    });
+};
+
+startPage();
